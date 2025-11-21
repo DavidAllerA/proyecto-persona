406d83b100cfb0fb69d52ebbd372081e64f70fdd
David <sonbikero@gmail.com>
2025-11-21 10:17:06 +0100

    Primer Commit

---
+++ persona.java
@@ -0,0 +1,19 @@
+
+public class persona
+{
+    // instance variables - replace the example below with your own
+    private int edad;
+    private String nombre;
+
+    /**
+     * Constructor for objects of class persona
+     */
+    public persona(int laEdad, String elNombre)
+    {
+        // initialise instance variables
+        edad = laEdad;
+        nombre = elNombre;
+    }
+
+   
+}
